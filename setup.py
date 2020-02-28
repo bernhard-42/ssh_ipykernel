12de86767a8a07deaf8a03dea96a6f858b507def
Bernhard Walter <b_walter@arcor.de>
2020-02-28 19:02:37 +0100

    formatted with black

--- setup.py
+++ setup.py
@@ -5,39 +5,39 @@
 
 from setuptools import setup, find_packages
 
-with open('README.md') as readme_file:
+with open("README.md") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.md') as history_file:
+with open("HISTORY.md") as history_file:
     history = history_file.read()
 
 setup(
     author="Bernhard Walter",
-    author_email='b_walter@arcor.de',
+    author_email="b_walter@arcor.de",
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
     description="A remote jupyter ipykernel via ssh",
     install_requires=[
         "pexpect==4.7.0",
         "tornado>=6.0.3",
-        "jupyter_client>=5.3.1,<5.4.0"
+        "jupyter_client>=5.3.1,<5.4.0",
     ],
     license="MIT license",
-    long_description=readme + '\n\n' + history,
-    long_description_content_type='text/markdown',
+    long_description=readme + "\n\n" + history,
+    long_description_content_type="text/markdown",
     include_package_data=True,
-    keywords='ssh_ipykernel',
-    name='ssh_ipykernel',
-    packages=find_packages(include=['ssh_ipykernel']),
-    python_requires='>=3.5',
-    url='https://github.com/bernhard-42/ssh_ipykernel',
-    version='0.9.4',
-    zip_safe=False
+    keywords="ssh_ipykernel",
+    name="ssh_ipykernel",
+    packages=find_packages(include=["ssh_ipykernel"]),
+    python_requires=">=3.5",
+    url="https://github.com/bernhard-42/ssh_ipykernel",
+    version="0.9.4",
+    zip_safe=False,
 )
