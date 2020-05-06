df5e6a7a12c85c2a101cf8bd14b12663087b4b1b
Bernhard Walter <bernhard.walter@databricks.com>
2020-05-06 18:43:02 +0200

    bumped jupyterlab version

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
         "pexpect==4.7.0",
         "tornado>=6.0.3",
         "jupyter_client>=5.3.1,<5.4.0",
-        "jupyterlab>=2.0.0,<2.1.0",
+        "jupyterlab>=2.1.0,<2.2.0",
     ],
     data_files=[
         (
