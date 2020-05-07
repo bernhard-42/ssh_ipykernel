d8a6041a82bb3209301c93175ed0ab3d82b168e6
Bernhard Walter <bernhard.walter@databricks.com>
2020-05-07 17:48:39 +0200

    Latest release: 1.0.0

--- setup.py
+++ setup.py
@@ -46,6 +46,6 @@ setup(
     # packages=find_packages(include=["ssh_ipykernel", "interrupt_ssh_ipykernel"]),
     python_requires=">=3.5",
     url="https://github.com/bernhard-42/ssh_ipykernel",
-    version="0.9.4",
+    version="1.0.0",
     zip_safe=False,
 )
